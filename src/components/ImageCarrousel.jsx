ce46f30441d5996203f86f2e50d91cbeed63ab66
wildfireOfMine <iamtheanguera@gmail.com>
2025-05-02 12:32:46 +0200

    Added ImageCarrousel.jsx

---
+++ src/components/ImageCarrousel.jsx
@@ -0,0 +1,12 @@
+import { Image } from '@mui/icons-material'
+import React from 'react'
+
+const ImageCarrousel = () => {
+  return (
+    <div>
+      <img src="../../imgs/italy.jpg"/>
+    </div>
+  )
+}
+
+export default ImageCarrousel
