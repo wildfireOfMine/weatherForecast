319dd71d3222a4fb95101824f36361f5aa7dfe43
wildfireOfMine <iamtheanguera@gmail.com>
2025-06-09 16:24:09 +0200

    New changes of responsiveness

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 
 const Logo = ({width=5, height=5}) => {
   return (
-    <img src='../../imgs/WeatherForecast.png' width={width} height={height} alt='Weather Forecast Logo'/>
+    <img src='../../imgs/WeatherForecast.png' width={width} height={height} alt='Weather Forecast Logo'
+    style={{
+      minHeight: "75px",
+      minWidth: "100px",
+    }}/>
   )
 }
 
